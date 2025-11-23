d61b937d90c34f0077d47cf655b782400222f4e2
Kavinda Rochana <kavindaa@m1.com.sg>
2025-11-23 14:02:01 +0800

    fix: firebase auth and firestore init config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './src/store';
 import AppNavigator from './src/navigation/AppNavigator';
 import { Text } from 'react-native';
+import { initializeFirebase } from './src/services/firebaseConfig';
 import './src/i18n';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    initializeFirebase();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
