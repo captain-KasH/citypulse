0532fe4b1b93c1ff743c02ce7519ed016594575a
Kavinda Rochana <kavindaa@m1.com.sg>
2025-11-23 18:03:27 +0800

    lint rules updated

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    ignores: ['.eslintrc.js', 'node_modules/**', 'android/**', 'ios/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+];
